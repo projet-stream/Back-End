9fd41a0e1aef29b11bc586037217141dc225aada
Gaël <lfg.lefortgael@gmail.com>
2021-05-06 16:16:17 +0200

    V2

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,28 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+router.post('/auth', async(req, res, next) => {
+  const { username, password } = req.body;
+  const user = await mongoose.model('User').findOne({
+    username,
+    password
+  })
+
+  if (user) {
+    res.json({
+      success: true,
+      message: 'Authentication successful!',
+      // token: token,
+      toker: user._id
+    });
+  } else {
+    res.send(403).json({
+      success: false,
+      message: 'Login failed'
+    });
+  } 
+})
+
 /* GET users listing. */
 router.get('/', async(req, res, next) => {
   res.json(await mongoose.model('User').find({}));
